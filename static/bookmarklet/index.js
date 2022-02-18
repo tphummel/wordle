42b3b42f111d6e382e7a4abb84309f7d55f9e054
Tom Hummel <tom.hummel@oracle.com>
2022-02-17 20:52:51 -0800

    organize bookmarklet files

--- static/bookmarklet/index.js
+++ static/bookmarklet/index.js
@@ -1,19 +1,17 @@
-javascript:(function(){
-  const stats = JSON.parse(window.localStorage.getItem("nyt-wordle-statistics"));
-  const state = JSON.parse(window.localStorage.getItem("nyt-wordle-state"));
+const stats = JSON.parse(window.localStorage.getItem("nyt-wordle-statistics"));
+const state = JSON.parse(window.localStorage.getItem("nyt-wordle-state"));
 
-  const completedAt = new Date(state.lastCompletedTs);
+const completedAt = new Date(state.lastCompletedTs);
 
-  const fileText = `---\n
+const fileText = `---\n
 title: ${state.solution}\n
 date: ${completedAt.toISOString()}\n
 state: ${JSON.stringify(state, null, 2)}\n
 stats: ${JSON.stringify(stats, null, 2)}
 \n---`;
 
-  const encodedFileText = encodeURIComponent(fileText);
-  const filename = `${state.solution}.md`;
-  const githubQueryLink = "https://github.com/tphummel/wordle-static/new/main/content/w/new?value=" + encodedFileText +"&filename=" + filename;
+const encodedFileText = encodeURIComponent(fileText);
+const filename = `${state.solution}.md`;
+const githubQueryLink = "https://github.com/tphummel/wordle-static/new/main/content/w/new?value=" + encodedFileText +"&filename=" + filename;
 
-  window.open(githubQueryLink);
-})();
+window.open(githubQueryLink);
