a3c8183993e1ed0bdc55215bbacc16a82d2d3984
Tom Hummel <tohu@hey.com>
2022-02-20 15:17:09 -0800

    add puzzle number to save template

--- static/bookmarklet/index.js
+++ static/bookmarklet/index.js
@@ -3,16 +3,36 @@ const state = JSON.parse(window.localStorage.getItem("nyt-wordle-state"));
 
 const completedAt = new Date(state.lastCompletedTs);
 
-const fileText = `---\n
-title: ${state.solution}\n
-date: ${completedAt.toISOString()}\n
-tags: []\n
-state: ${JSON.stringify(state, null, 2)}\n
+const epoch = new Date("2021-06-19T00:00:00");
+const solutionCount = 2315;
+
+function getDaysBetween(start, end) {
+  let startDate = new Date(start);
+  let daysBetween = new Date(end).setHours(0, 0, 0, 0) - startDate.setHours(0, 0, 0, 0);
+  return Math.floor(daysBetween / 864e5)
+}
+
+function getPuzzleNumber(today) {
+  let puzzleNumber = getDaysBetween(epoch, today) % solutionCount
+  return puzzleNumber
+}
+
+let puzzleNumber = getPuzzleNumber(new Date)
+
+const fileText = `---
+title: ${state.solution}
+date: ${completedAt.toISOString()}
+puzzle: ${puzzleNumber}
+tags: []
+state: ${JSON.stringify(state, null, 2)}
 stats: ${JSON.stringify(stats, null, 2)}
-\n---`;
+---
+
+<!-- more -->
+`;
 
 const encodedFileText = encodeURIComponent(fileText);
-const filename = `${state.solution}.md`;
-const githubQueryLink = "https://github.com/tphummel/wordle-static/new/main/content/w/new?value=" + encodedFileText +"&filename=" + filename;
+const filename = `${puzzleNumber}.md`;
+const githubQueryLink = "https://github.com/tphummel/wordle-static/new/main/content/w/new?quick_pull=1&labels=puzzle&value=" + encodedFileText +"&filename=" + filename;
 
 window.open(githubQueryLink);
