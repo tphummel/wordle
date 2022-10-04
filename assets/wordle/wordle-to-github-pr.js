b04f526150a940dd85a1ece894fc90df1803eea3
Tom Hummel <tohu@hey.com>
2022-10-03 21:58:19 -0700

    Standard-ize moved js files

--- assets/wordle/wordle-to-github-pr.js
+++ assets/wordle/wordle-to-github-pr.js
@@ -1,69 +1,63 @@
-const stats = JSON.parse(window.localStorage.getItem("nyt-wordle-statistics"));
-const state = JSON.parse(window.localStorage.getItem("nyt-wordle-state"));
+const stats = JSON.parse(window.localStorage.getItem('nyt-wordle-statistics'))
+const state = JSON.parse(window.localStorage.getItem('nyt-wordle-state'))
 
-const epoch = new Date("2021-06-19T00:00:00");
-const solutionCount = 2309;
+const epoch = new Date('2021-06-19T00:00:00')
+const solutionCount = 2309
 
-function getDaysBetween(start, end) {
-  let startDate = new Date(start);
-  let daysBetween = new Date(end).setHours(0, 0, 0, 0) - startDate.setHours(0, 0, 0, 0);
+function getDaysBetween (start, end) {
+  const startDate = new Date(start)
+  const daysBetween = new Date(end).setHours(0, 0, 0, 0) - startDate.setHours(0, 0, 0, 0)
   return Math.floor(daysBetween / 864e5)
 }
 
-function getPuzzleNumber(today) {
-  let puzzleNumber = getDaysBetween(epoch, today) % solutionCount
+function getPuzzleNumber (today) {
+  const puzzleNumber = getDaysBetween(epoch, today) % solutionCount
   return puzzleNumber
 }
 
 // from: https://usefulangle.com/post/30/javascript-get-date-time-with-offset-hours-minutes
 function getLocalTimeZone () {
-  var timezone_offset_min = new Date().getTimezoneOffset(),
-    offset_hrs = parseInt(Math.abs(timezone_offset_min/60), 10),
-    offset_min = Math.abs(timezone_offset_min%60),
-    timezone_standard;
-
-  if(offset_hrs < 10)
-    offset_hrs = '0' + offset_hrs;
-
-  if(offset_min < 10)
-    offset_min = '0' + offset_min;
-
-  if(timezone_offset_min < 0)
-    timezone_standard = '+' + offset_hrs + ':' + offset_min;
-  else if(timezone_offset_min > 0)
-    timezone_standard = '-' + offset_hrs + ':' + offset_min;
-  else if(timezone_offset_min == 0)
-    timezone_standard = 'Z';
-
-  return timezone_standard
+  const timezoneOffsetMin = new Date().getTimezoneOffset()
+  let offsetHours = parseInt(Math.abs(timezoneOffsetMin / 60), 10)
+  let offsetMin = Math.abs(timezoneOffsetMin % 60)
+  let timezoneStandard
+  if (offsetHours < 10) { offsetHours = '0' + offsetHours }
+  if (offsetMin < 10) { offsetMin = '0' + offsetMin }
+  if (timezoneOffsetMin < 0) {
+    timezoneStandard = '+' + offsetHours + ':' + offsetMin
+  } else if (timezoneOffsetMin > 0) {
+    timezoneStandard = '-' + offsetHours + ':' + offsetMin
+  } else if (timezoneOffsetMin === 0) {
+    timezoneStandard = 'Z'
+  }
+  return timezoneStandard
 }
 
 function getDateTime (dateStr) {
-  var dt = new Date(dateStr),
-    current_date = dt.getDate(),
-    current_month = dt.getMonth() + 1,
-    current_year = dt.getFullYear(),
-    current_hrs = dt.getHours(),
-    current_mins = dt.getMinutes(),
-    current_secs = dt.getSeconds(),
-    current_datetime;
+  const dt = new Date(dateStr)
+  let currentDate = dt.getDate()
+  let currentMonth = dt.getMonth() + 1
+  const currentYear = dt.getFullYear()
+  let currentHrs = dt.getHours()
+  let currentMins = dt.getMinutes()
+  let currentSecs = dt.getSeconds()
 
-  current_date = current_date < 10 ? '0' + current_date : current_date;
-  current_month = current_month < 10 ? '0' + current_month : current_month;
-  current_hrs = current_hrs < 10 ? '0' + current_hrs : current_hrs;
-  current_mins = current_mins < 10 ? '0' + current_mins : current_mins;
-  current_secs = current_secs < 10 ? '0' + current_secs : current_secs;
+  currentDate = currentDate < 10 ? '0' + currentDate : currentDate
+  currentMonth = currentMonth < 10 ? '0' + currentMonth : currentMonth
+  currentHrs = currentHrs < 10 ? '0' + currentHrs : currentHrs
+  currentMins = currentMins < 10 ? '0' + currentMins : currentMins
+  currentSecs = currentSecs < 10 ? '0' + currentSecs : currentSecs
 
-  current_datetime = current_year + '-' + current_month + '-' + current_date + 'T' + current_hrs + ':' + current_mins + ':' + current_secs;
-  return current_datetime
+  const currentDatetime = currentYear + '-' + currentMonth + '-' + currentDate + 'T' + currentHrs + ':' + currentMins + ':' + currentSecs
+  return currentDatetime
 }
 
-let puzzleNumber = getPuzzleNumber(new Date)
-let puzzleDate = getDateTime(state.lastCompletedTs).substring(0,10)
+const puzzleNumber = getPuzzleNumber(new Date())
+const puzzleDate = getDateTime(state.lastCompletedTs).substring(0, 10)
 
 const fileText = `---
 title: "${puzzleNumber}: ${puzzleDate}"
-date: ${getDateTime(state.lastCompletedTs)+getLocalTimeZone()}
+date: ${getDateTime(state.lastCompletedTs) + getLocalTimeZone()}
 tags: []
 words: ${JSON.stringify(state.boardState.filter(w => w !== ''))}
 puzzles: [${puzzleNumber}]
@@ -72,10 +66,10 @@ stats: ${JSON.stringify(stats, null, 2)}
 ---
 
 <!-- more -->
-`;
+`
 
-const encodedFileText = encodeURIComponent(fileText);
-const filename = `${puzzleDate}.md`;
-const githubQueryLink = "https://github.com/tphummel/wordle/new/main/content/w/new?quick_pull=1&labels=puzzle&value=" + encodedFileText +"&filename=" + filename;
+const encodedFileText = encodeURIComponent(fileText)
+const filename = `${puzzleDate}.md`
+const githubQueryLink = 'https://github.com/tphummel/wordle/new/main/content/w/new?quick_pull=1&labels=puzzle&value=' + encodedFileText + '&filename=' + filename
 
-window.open(githubQueryLink);
+window.open(githubQueryLink)
--- assets/wordle/wordle-to-json.js
+++ assets/wordle/wordle-to-json.js
@@ -1,27 +1,25 @@
-var stats = JSON.parse(window.localStorage.getItem("nyt-wordle-statistics"));
-var state = JSON.parse(window.localStorage.getItem("nyt-wordle-state"));
+const stats = JSON.parse(window.localStorage.getItem('nyt-wordle-statistics'))
+const state = JSON.parse(window.localStorage.getItem('nyt-wordle-state'))
 
-var completedAt = new Date(state.lastCompletedTs);
-
-var epoch = new Date("2021-06-19T00:00:00");
-var solutionCount = 2309;
+const epoch = new Date('2021-06-19T00:00:00')
+const solutionCount = 2309
 
 function getDaysBetween (start, end) {
-  var startDate = new Date(start);
-  var daysBetween = new Date(end).setHours(0, 0, 0, 0) - startDate.setHours(0, 0, 0, 0);
+  const startDate = new Date(start)
+  const daysBetween = new Date(end).setHours(0, 0, 0, 0) - startDate.setHours(0, 0, 0, 0)
   return Math.floor(daysBetween / 864e5)
 }
 
-function getPuzzleNumber(today) {
-  var puzzleNumber = getDaysBetween(epoch, today) % solutionCount
+function getPuzzleNumber (today) {
+  const puzzleNumber = getDaysBetween(epoch, today) % solutionCount
   return puzzleNumber
 }
-var now = new Date
-var puzzleNumber = getPuzzleNumber(now)
+const now = new Date()
+const puzzleNumber = getPuzzleNumber(now)
 
-alert(JSON.stringify({
+window.alert(JSON.stringify({
   puzzle: puzzleNumber,
   now,
   state,
   stats
-}, null, 2));
+}, null, 2))
