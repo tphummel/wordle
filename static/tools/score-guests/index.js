632c0792d0c3a9d663ff78dd41d1a161c992f9dc
Tom Hummel <tohu@hey.com>
2023-01-01 14:47:38 -0800

    Add raw share to guest data. Output reporting at the end

--- static/tools/score-guests/index.js
+++ static/tools/score-guests/index.js
@@ -22,14 +22,33 @@ fs.createReadStream(filename)
       return Object.assign(enhanced, {
         playerName: lowercaseName,
         timestamp: player.ts,
+        rawShare: player.result,
         words: player.words
           .split(/[\s\,]/)
           .filter(w => w.length === 5)
           .map(w => w.toLowerCase())
       })
+    }).sort((a, b) => {
+      if (a.puzzleScore > b.puzzleScore) return 1
+      if (a.puzzleScore < b.puzzleScore) return -1
+      return 0
     })
 
     enhancedPlayers.forEach((player) => {
       fs.writeFileSync(`./${player.playerName}.json`, JSON.stringify(player, null, 2))
+    })
+
+    enhancedPlayers.forEach((p) => {
+      console.log(`${p.playerName}: ${p.puzzleScore} Points`)
+    })
+
+    enhancedPlayers.forEach((p) => {
+      console.log(`
+Name: ${p.playerName}
+Score: ${p.puzzleScore}
+Words: ${p.words.join(', ')}
+${p.rawShare}
+----
+`)
     })
   })
