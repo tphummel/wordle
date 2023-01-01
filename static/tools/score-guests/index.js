f49165dc59eb1a269b2759788a01b89b32153cb2
Tom Hummel <tohu@hey.com>
2023-01-01 14:30:54 -0800

    Parse tsv from google sheets and process into guest player json files

---
+++ static/tools/score-guests/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const path = require('path')
+const csv = require('csv-parser')
+const analyzeEmoji = require(path.join(__dirname, '../enhance/enrich-emoji.js'))
+
+// TODO: load my puzzle file to get the starter and solution words. remove those from the guest word lists if they have been included.
+
+const filename = process.argv[2]
+
+const players = []
+fs.createReadStream(filename)
+  .pipe(csv({ 
+    separator: '\t',
+    headers: ['ts', 'name', 'result', 'words']
+  }))
+  .on('data', (data) => players.push(data))
+  .on('end', () => {
+    const enhancedPlayers = players.map((player, i) => {
+      const enhanced = analyzeEmoji(player.result)
+      const lowercaseName = player.name.toLowerCase()
+
+      return Object.assign(enhanced, {
+        playerName: lowercaseName,
+        timestamp: player.ts,
+        words: player.words
+          .split(/[\s\,]/)
+          .filter(w => w.length === 5)
+          .map(w => w.toLowerCase())
+      })
+    })
+
+    enhancedPlayers.forEach((player) => {
+      fs.writeFileSync(`./${player.playerName}.json`, JSON.stringify(player, null, 2))
+    })
+  })
